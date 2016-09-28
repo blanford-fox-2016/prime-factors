cdec34a6d9f44e141b7e6a4a6d8e2085a181b12b
Septian A Tama <tama@tamatamvan.web.id>
2016-09-28 19:55:41 +0700

    prime factor and simple recursive - Tama
    prime factor and simple recursive - Tama

--- recursive.js
+++ recursive.js
@@ -3,6 +3,19 @@
 // release 0
 let prime_factors = (angka, hasil = []) => {
   // write your code here
+  var hasil = [];
+  for (var i = 2; i < angka; i++) {
+    while (angka % i === 0) {
+      angka = angka/i;
+      hasil.push(i);
+    }
+  }
+
+  if(angka > 1) {
+    hasil.push(i);
+  }
+
+  return(hasil);
 }
 
 console.log(prime_factors(3)); // [3]
@@ -14,6 +27,18 @@ console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
 // release 1
 let simple_recursive = (number) => {
   // write your code here
+  var countNum = number.toString().split("");
+  if (countNum.length === 1) {
+    return countNum.join("");
+  }
+  else if (countNum.length > 1) {
+    var sum = 1;
+    for (var i = 0; i < countNum.length; i++) {
+      sum *= countNum[i];
+    }
+    return simple_recursive(sum)
+  }
+
 }
 
 console.log(simple_recursive(39)); // 4
