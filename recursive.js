3e9268e920ead935aabac459df9f2998198cb797
broerjuang <juangwiantoro@gmail.com>
2016-10-13 17:19:18 +0700

    release 0

--- recursive.js
+++ recursive.js
@@ -1,21 +1,42 @@
 "use strict";
 
 // release 0
-let prime_factors = (angka, hasil = []) => {
-  // write your code here
-}
+let prime_factors = ((num) => {
+  let result = [];
+  for (var i = 2; i <= num / i; i += 1) {
+    while (num % i === 0) {
+      num /= i;
+      result.push(i);
+    }
+  }
+  num > 1 ? result.push(num) :false;
+  return result;
+});
+
 
 console.log(prime_factors(3)); // [3]
 console.log(prime_factors(6)); // [2,3]
 console.log(prime_factors(8)); // [2,2,2]
 console.log(prime_factors(25)); // [5,5]
-console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
+//console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
 
 // release 1
-let simple_recursive = (number) => {
-  // write your code here
-}
 
-console.log(simple_recursive(39)); // 4
-console.log(simple_recursive(999)); // 2
-console.log(simple_recursive(3)); // 3
+// let simple_recursive = (number) => {
+//   let temp = String(number).split("").map(Number);
+//   if (temp.length === 1) {
+//     return temp[0];
+//   } else {
+//     temp.reduce((prev, cur) => {
+//       prev * cur
+//     })
+//     return simple_recursive(number);
+//   }
+//
+//   // write your code here
+// }
+//
+//
+// console.log(simple_recursive(39)); // 4
+// console.log(simple_recursive(999)); // 2
+// console.log(simple_recursive(3)); // 3
