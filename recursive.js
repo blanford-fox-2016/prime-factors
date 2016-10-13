c878b84b9b37c2666a2f6c333688f1bace33a20f
Izhharuddin Syanmil <email": null>
2016-10-13 17:55:08 +0700

    test re-clone

--- recursive.js
+++ recursive.js
@@ -2,8 +2,20 @@
 
 // release 0
 let prime_factors = (angka, hasil = []) => {
-  // write your code here
-}
+  var x = 2;
+    while (angka != 1) {
+      var temp = angka % x;
+      if (temp === 0) {
+        hasil.push(x);
+        angka = angka / x;
+        x = 2;
+      } else{
+        x = x + 1;
+      }
+    }
+      return(hasil);
+  }
+
 
 console.log(prime_factors(3)); // [3]
 console.log(prime_factors(6)); // [2,3]
@@ -11,11 +23,11 @@ console.log(prime_factors(8)); // [2,2,2]
 console.log(prime_factors(25)); // [5,5]
 console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
 
-// release 1
-let simple_recursive = (number) => {
-  // write your code here
-}
-
-console.log(simple_recursive(39)); // 4
-console.log(simple_recursive(999)); // 2
-console.log(simple_recursive(3)); // 3
+// // release 1
+// let simple_recursive = (number) => {
+//   // write your code here
+// }
+//
+// console.log(simple_recursive(39)); // 4
+// console.log(simple_recursive(999)); // 2
+// console.log(simple_recursive(3)); // 3
