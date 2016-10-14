256979808f0f0a6e24ead23cbd025a6295312057
Alessandro C <alessandro.christopher@gmail.com>
2016-10-14 10:45:48 +0700

    Submitted

--- recursive.js
+++ recursive.js
@@ -1,8 +1,26 @@
 "use strict";
 
 // release 0
-let prime_factors = (angka, hasil = []) => {
-  // write your code here
+let prime_factors = (number) => {
+  var output = [], divisor = 2
+  if (number == 1){
+    output.push(1)
+  }
+  if(number < 1){
+    console.log("Number must be greater than 1");
+  }
+  else{
+    while(number >1){ //3
+      if (number % divisor == 0){
+        number = number / divisor
+        output.push(divisor)
+      }
+      else{
+        divisor++
+      }
+    }
+  }
+  return output
 }
 
 console.log(prime_factors(3)); // [3]
@@ -13,7 +31,18 @@ console.log(prime_factors(123123123)); // [3, 3, 41, 333667]
 
 // release 1
 let simple_recursive = (number) => {
-  // write your code here
+  var arr = number.toString().split('')
+  if (arr.length > 1){
+    var temp = 1
+    for (var i = 0; i < arr.length; i++) {
+      temp = temp * parseInt(arr[i])
+    }
+  }
+  else{
+    return number
+
+  }
+    return simple_recursive(temp)
 }
 
 console.log(simple_recursive(39)); // 4
